eb4c5163ab8fa57f82924ca2d35c681416d91398
Воронцов Олег Валерьевич <vorontsov@root.ians.aero>
2019-10-29 14:27:39 +0300

    Инициализатор json, подхват json, алгоритм с данными

---
+++ GUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TelemetryEngine.TelemetryEngine.ShowData(TelemetryEngine.TelemetryEngine.m1);
+            Console.WriteLine(new string('-',50));
+            TelemetryEngine.TelemetryEngine.ShowData(TelemetryEngine.TelemetryEngine.mW);
+
+            TelemetryEngine.TelemetryEngine.Start();
+            TelemetryEngine.TelemetryEngine.ShowData(TelemetryEngine.TelemetryEngine.mRes);
+        }
+    }
+}
